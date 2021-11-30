af5d1980707b261412da35266473280472a29238
Sanaz <sanaz.azh@gmail.com>
2021-11-30 09:15:54 -0500

    override move class in dog and call move either from the class or from super in other Dog methods(run, walk)

--- Inheritance/src/com/sanaz/Dog.java
+++ Inheritance/src/com/sanaz/Dog.java
@@ -32,11 +32,22 @@ public class Dog extends Animal {
 
     public void walk() {
         System.out.println("Dog.walk() is called");
-        move(5);
+        super.move(5);
     }
 
     public void run(){
         System.out.println("Dog.run() is called");
         move(10);
+    }
+
+    private void moveLegs(int speed){
+        System.out.println("Dog.moveLegs() is called");
+    }
+
+    @Override
+    public void move(int speed) {
+        System.out.println("Dog.move() called.");
+        moveLegs(speed);
+        super.move(speed);
     }
 }
