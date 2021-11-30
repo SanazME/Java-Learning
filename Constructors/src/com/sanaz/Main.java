f0c33528c87cc156133c7ec572285b4757e3050a
Sanaz <sanaz.azh@gmail.com>
2021-11-30 07:23:35 -0500

    VipCustomer exercise

--- Constructors/src/com/sanaz/Main.java
+++ Constructors/src/com/sanaz/Main.java
@@ -22,5 +22,21 @@ public class Main {
         System.out.println(bank1.getBalance());
 
         BankAccount timAccount = new BankAccount("Tim","timEmail@", "somePhone123234");
+
+        /*
+        * // Create a new class VipCustomer
+        // it should have 3 fields name, credit limit, and email address.
+        // create 3 constructors
+        // 1st constructor empty should call the constructor with 3 parameters with default values
+        // 2nd constructor should pass on the 2 values it receives and add a default value for the 3rd
+        // 3rd constructor should save all fields.
+        // create getters only for this using code generation of intellij as setters wont be needed
+        // test and confirm it works.
+        * */
+        VipCustomer person1 = new VipCustomer();
+        System.out.println(person1.getName());
+
+        VipCustomer person2 = new VipCustomer("Alice", "alice@email");
+        System.out.println(person2.getName());
     }
 }
---
+++ Constructors/src/com/sanaz/VipCustomer.java
@@ -0,0 +1,33 @@
+package com.sanaz;
+
+public class VipCustomer {
+    private String name;
+    private double creditLimit;
+    private String email;
+
+    public VipCustomer(String name, double creditLimit, String email) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
+    }
+
+    public VipCustomer() {
+        this("uknown", 0.0, "unknown@gmail.com");
+    }
+
+    public VipCustomer(String name, String email) {
+        this(name, 0.0, email);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCreditLimit() {
+        return creditLimit;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
