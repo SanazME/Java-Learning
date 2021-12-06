3b6e247760c96ba32cd3ae2882c480b412680c44
Sanaz <sanaz.azh@gmail.com>
2021-12-06 08:44:06 -0500

    Encapsulation

--- Encapsulation/src/com/sanaz/Main.java
+++ Encapsulation/src/com/sanaz/Main.java
@@ -3,17 +3,23 @@ package com.sanaz;
 public class Main {
 
     public static void main(String[] args) {
-	    Player player = new Player();
-        player.name = "Tim";
-        player.health = 20;
-        player.weapon = "Sword";
+//	    Player player = new Player();
+//        player.name = "Tim";
+//        player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health =  " + player.healthRemaining());
+//
+//        damage = 11;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health =  " + player.healthRemaining());
 
-        int damage = 10;
-        player.loseHealth(damage);
-        System.out.println("Remaining health =  " + player.healthRemaining());
+        EnhancedPlayer player = new EnhancedPlayer("Tim", 50, "sword");
+        System.out.println("Initial health is " + player.getHealth());
 
-        damage = 11;
-        player.loseHealth(damage);
-        System.out.println("Remaining health =  " + player.healthRemaining());
+        EnhancedPlayer player2 = new EnhancedPlayer("Tim", 300, "sword");
+        System.out.println("Initial health is " + player2.getHealth());
     }
 }
