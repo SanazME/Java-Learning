3216cfa5f42f01343c27d915a7473cbe4d13e7ae
Sanaz <sanaz.azh@gmail.com>
2021-12-12 16:02:01 -0500

    ReverseArrayChallenge approach 2

--- ReverseArrayChallenge/src/com/sanaz/Main.java
+++ ReverseArrayChallenge/src/com/sanaz/Main.java
@@ -19,8 +19,24 @@ import java.util.Arrays;
 
 public class Main {
 
+    private static void reverse(int[] arr){
+        int size = arr.length;
+        int stopIdx = size / 2;
+
+        System.out.println("Array before: " + Arrays.toString(arr));
+
+        int tmp;
+        for (int i=0; i < stopIdx; i++){
+            tmp = arr[i];
+            arr[i] = arr[size - i - 1];
+            arr[size - i -1] = tmp;
+        }
+
+        System.out.println("Array after: " + Arrays.toString(arr));
+    }
+
     public static void main(String[] args) {
         int[] arr1= {1,2,3};
-        ReverseArray.reverse(arr1);
+        reverse(arr1);
     }
 }
