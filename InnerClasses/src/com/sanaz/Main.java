f006d65f7daad7110d878a5c702725fa0bc07986
Sanaz <sanaz.azh@gmail.com>
2022-01-04 09:19:08 -0500

    Inner class change to private

--- InnerClasses/src/com/sanaz/Main.java
+++ InnerClasses/src/com/sanaz/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         Gearbox gearbox = new Gearbox(6);
-        Gearbox.Gear first = gearbox.new Gear(1, 12.3);
+//        Gearbox.Gear first = gearbox.new Gear(1, 12.3);
 
-        System.out.println(first.driveSpeed(1800));
+//        System.out.println(first.driveSpeed(1800));
     }
 }
