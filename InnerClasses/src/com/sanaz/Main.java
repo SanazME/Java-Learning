910d7f962dc3f7e2ba8fce5686d4dadc97dc3e25
Sanaz <sanaz.azh@gmail.com>
2022-01-04 09:12:31 -0500

    Inner class public

---
+++ InnerClasses/src/com/sanaz/Main.java
@@ -0,0 +1,11 @@
+package com.sanaz;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Gearbox gearbox = new Gearbox(6);
+        Gearbox.Gear first = gearbox.new Gear(1, 12.3);
+
+        System.out.println(first.driveSpeed(1800));
+    }
+}
