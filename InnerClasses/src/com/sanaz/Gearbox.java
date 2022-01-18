41e11fc2c73607f1dc5a4ad662cacc22f6c89690
Sanaz <sanaz.azh@gmail.com>
2022-01-18 08:30:30 -0500

    addGear method is moved to contructor for gearbox because adding gears is part of creating a gearbox

--- InnerClasses/src/com/sanaz/Gearbox.java
+++ InnerClasses/src/com/sanaz/Gearbox.java
@@ -13,13 +13,18 @@ public class Gearbox {
         this.gears = new ArrayList<>();
         Gear neutral = new Gear(0, 0.0);
         this.gears.add(neutral);
+
+        // add addGear method as part of contructor because when we create a gearbox, we need to add gears
+        for (int i=0; i < maxGears; i++){
+            addGear(i, i * 5.3);
+        }
     }
 
     public void operateClutch(boolean in){
         this.clutchIsIn = in;
     }
 
-    public void addGear(int number, double ratio){
+    private void addGear(int number, double ratio){
         if ((number > 0 ) && (number <= maxGears)){
             this.gears.add(new Gear(number, ratio));
         }
--- InnerClasses/src/com/sanaz/Main.java
+++ InnerClasses/src/com/sanaz/Main.java
@@ -4,9 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         Gearbox gearbox = new Gearbox(6);
-        gearbox.addGear(1, 5.3);
-        gearbox.addGear(2, 10.6);
-        gearbox.addGear(3, 15.9);
         gearbox.operateClutch(true);
         gearbox.changeGear(1);
         gearbox.operateClutch(false);
