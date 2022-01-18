73099d2776dac0dc45810534e1990bca596c2250
Sanaz <sanaz.azh@gmail.com>
2022-01-18 08:46:12 -0500

    Local Inner classes

--- InnerClasses/src/com/sanaz/Main.java
+++ InnerClasses/src/com/sanaz/Main.java
@@ -1,19 +1,40 @@
 package com.sanaz;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
 
     public static void main(String[] args) {
-        Gearbox gearbox = new Gearbox(6);
-        gearbox.operateClutch(true);
-        gearbox.changeGear(1);
-        gearbox.operateClutch(false);
-        System.out.println(gearbox.wheelSpeed(1000));
-        gearbox.changeGear(2);
-        System.out.println(gearbox.wheelSpeed(3000));
-        gearbox.operateClutch(true);
-        gearbox.changeGear(3);
-        gearbox.operateClutch(false);
-        System.out.println(gearbox.wheelSpeed(6000));
+        class ClickListener implements Button.OnClickListener{
+            public ClickListener() {
+                System.out.println("I've been attached.");
+            }
+
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked!");
+            }
+
+        }
+
+        btnPrint.setOnClickListener(new ClickListener());
+        listen();
+    }
+
+    private static void listen(){
+        boolean quit = false;
+        while (!quit){
+            int choice = scanner.nextInt();
+            switch (choice){
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    btnPrint.onClick();
+            }
+        }
     }
 
 }
